b763f0fb241043bcc35d78c2b22e6d38de4d195a
Dornavan <130479724+dorndickence@users.noreply.github.com>
2024-05-17 01:46:53 -0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -15,14 +15,14 @@ const About = () => {
         Hello, I'm{" "}
         <span className='blue-gradient_text font-semibold drop-shadow'>
           {" "}
-          Adrian
+          Dorn
         </span>{" "}
         👋
       </h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-          Software Engineer based in Croatia, specializing in technical
+          A web developer/data analyst  based in kenya, Am 20 years old and am specializing in technical
           education through hands-on learning and building applications.
         </p>
       </div>
